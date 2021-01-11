3b376a92e722cb8e7498c0f8c96f2196a30ce3f6
Lufrac <rbuerg@googlemail.com>
2021-01-11 14:15:58 +0100

    Change to appointment skill
    Change all to appointmentskills

--- __init__.py
+++ __init__.py
@@ -1,15 +1,15 @@
 from mycroft import MycroftSkill, intent_file_handler
 
 
-class Hwskilltest(MycroftSkill):
+class NextAppointment(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
 
-    @intent_file_handler('hwskilltest.intent')
-    def handle_hwskilltest(self, message):
-        self.speak_dialog('hwskilltest')
+    @intent_file_handler('nextappointment.intent')
+    def handle_nextappointment(self, message):
+        self.speak_dialog('nextappointment')
 
 
 def create_skill():
-    return Hwskilltest()
+    return NextAppointment()
 
